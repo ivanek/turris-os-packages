c091acc4c817be385d535ec31dd0e1199ccd582a
Jan Pavlinec <jan.pavlinec@nic.cz>
2020-01-30 13:33:34 +0100

    nextcloud: fix nextcloud trusted domains script

--- net/nextcloud/files/nextcloud_disable_trusted_domains.sh
+++ net/nextcloud/files/nextcloud_disable_trusted_domains.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 # This script disables trusted domain check for nextcloud
-# by pushing * to tursted domain list
+# by pushing * to trusted domain list
+
+NEXTCLOUD_CLI_SCRIPT="/srv/www/nextcloud/occ"
+
+nextcloud_cli() {
+	sudo -u nobody php-cli $NEXTCLOUD_CLI_SCRIPT "$@"
+}
 
 if [ ! -f "/srv/www/nextcloud/occ" ]; then
 	echo "Error! occ command not found!"
 	exit 1
 fi
 
-sudo -u nobody php-cli /srv/www/nextcloud/occ config:system:get trusted_domains|grep '*'
-if [ "$?" = "0" ]; then
+# check if * is in config.php
+ret_val=$(nextcloud_cli config:system:get trusted_domains|awk '{ if ($1 == "*") {print "isDisabled" ;exit 1} }')
+if [ "$ret_val" = "isDisabled" ]; then
 	echo "Trusted domains already disabled.Nothing to do."
 else
-	echo "Disabling tursted domains."
-	sudo -u nobody php-cli /srv/www/nextcloud/occ config:system:set trusted_domains 1000 --value='*'
+	echo "Disabling trusted domains."
+	nextcloud_cli config:system:set trusted_domains 1000 --value='*'
 fi
